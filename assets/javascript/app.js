e1cd4790cf54450d608b5e6b414844d664fba517
Yewbdar <Yewbdar.grima@gmail.com>
2018-02-25 19:36:30 -0600

    updated app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,97 +1,69 @@
 $(document).ready(function () {
-  
+
     $("#form").hide();
     $("#result").hide();
 
     var trivia = {
 
-        second:15,
+        second: 10,
         intervalId: null,
-        Answer: ["1", "2", "4", "3", "1", "2", "3", "1", "4", "4"],
+        Answer: ["1", "1", "3", "4", "1", "3", "2", "1", "4", "4"],
         questions: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
         inCorrectAnswer: 0,
         correctAnswer: 0,
         notAnswered: 0,
         submit: true,
-        // displaySecond: function () {
-        //     localVal--;
-        //     console.log(localVal);
-        //     $("#time").text(this.second);
         
-        //     if(this.second<=0){
-        //         this.result();
-        //     }
-        // },
-        getInterval: function () {
+        getResult: function () {
             debugger;
-            var localVal = this.second;
-            clearInterval(this.intervalId);
-            
-            this.intervalId=setInterval(function(){
-                localVal--;
-                $("#time").text(localVal);
-                console.log(localVal);
-                if(localVal===0)
-                {
-                    debugger;
-                    clearInterval(this.intervalId);
-                    this.result(); 
-                    
-                }
-            }, 1000);
-        },
-        result: function () {
             clearInterval(this.intervalId);
             for (var i = 0; i < this.questions.length; i++) {
-
                 var selectedVal = "";
                 var selected = $("input[type='radio'][name='" + this.questions[i] + "']:checked");
                 if (selected.length > 0) {
                     selectedVal = selected.val();
                     if (selectedVal === this.Answer[i]) {
                         this.correctAnswer++;
-
                     }
                     else {
                         this.inCorrectAnswer++;
-
                     }
-
                 }
                 else {
                     this.notAnswered++;
-
                 }
-
             }
-
-            $('#correct-answert').text(this.correctAnswer);
+            $('#correct-answer').text(this.correctAnswer);
             $('#inCorrect-answer').text(this.inCorrectAnswer);
             $('#not-answer').text(this.notAnswered);
-
             $("#result").show();
             $("#form").hide();
-            this.submit=false;
+            this.submit = false;
         },
     }
-    // $('#time').html(this.second);
+    function startInteral() {
+        clearInterval(trivia.intervalId);
+        trivia.intervalId = setInterval(displaySecond, 1000);
+    }
+    function displaySecond() {
+        debugger;
+        trivia.second--;
+        // console.log(localVal);
+        $("#time").text("0:"+trivia.second);
 
+        if (trivia.second === 0) {
+            trivia.getResult();
+        }
+    }
     $('#start').on('click', function () {
-        
-        trivia.getInterval();
+         startInteral();
         $('form').show();
     })
     $('#submit').on('click', function () {
-        
         if (trivia.submit) {
-            trivia.result();
-
-            
+            trivia.getResult();
         }
-    });
-
-
+    })
 })
-
 
 
